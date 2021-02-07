e94b7785b479ff141bba9efb6477a77f11b2f83c
Eduardo Melo <eduardo.melo01992@gmail.com>
2021-02-06 21:44:39 -0300

    Aplicando eventos usando a função split e inserindo o switch

--- scripts/controller/calculatorController.js
+++ scripts/controller/calculatorController.js
@@ -2,6 +2,7 @@ class CalculatorController {
 
     constructor() {
 
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalculatorEl = document.querySelector('#display');
         this._dateEl = document.querySelector('#data');
@@ -23,18 +24,108 @@ class CalculatorController {
         }, 1000);
     }
 
+    addEventListenerAll(element, events, fn) {
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        });
+    }
+
+
+    clearAll() {
+
+        this._operation = [];
+
+    }
+
+    clearEntry() {
+
+        this._operation.pop();
+
+    }
+
+    addOperation(value) {
+
+        this._operation.push(value);
+
+    }
+
+    setError() {
+
+        this.displayCalculator = 'Error';
+
+    }
+
+    execBtn(value) {
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+
+                break;
+            case 'subtracao':
+
+                break;
+            case 'divisao':
+
+                break;
+            case 'multiplicacao':
+
+                break;
+            case 'porcento':
+
+                break;
+            case 'igual':
+
+                break;
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+
+        }
+
+    }
+
     initButtonsEvents() {
 
         const buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
         buttons.forEach((btn, index) => {
 
-            btn.addEventListener('click', e => {
+            this.addEventListenerAll(btn, 'click drag', e => {
 
-                console.log(btn.className.baseVal.replace('btn-', ''));
+                const textBtn = btn.className.baseVal.replace('btn-', '');
+
+                this.execBtn(textBtn);
 
             });
-        })
+
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+
+                btn.style.cursor = 'pointer';
+
+            });
+        });
     }
 
     setDisplayDateTime() {
